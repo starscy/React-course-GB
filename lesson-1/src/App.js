c256fdfed30f9587724cd8b21a883915f9e2f5ea
Vadim Karavaev <starscy@rambler.ru>
2022-05-13 13:19:54 +0300

    lesson-3 rebug

--- lesson-1/src/App.js
+++ lesson-1/src/App.js
@@ -1,29 +1,10 @@
-import { useState } from "react";
 import "./App.css";
 import Message from "./Message";
 
 function App(props) {
   const text = "First lesson message";
-  const [likes, setLikes] = useState(0);
-  const [value, setValue] = useState("Текст в инпуте");
-  function increment() {
-    setLikes(likes + 1);
-  }
-  function dicrement() {
-    setLikes(likes - 1);
-  }
   return (
     <div className="App">
-      <h1>{likes}</h1>
-      <h1>{value}</h1>
-      <input
-        type="text"
-        value={value}
-        onChange={(event) => setValue(event.target.value)}
-      />
-
-      <button onClick={increment}>increase</button>
-      <button onClick={dicrement}>dicrease</button>
       <Message name={text} />
     </div>
   );
