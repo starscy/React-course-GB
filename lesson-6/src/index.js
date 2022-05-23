6cb3f902142efa0244f8238bfae4ca35ba291e8a
Vadim Karavaev <starscy@rambler.ru>
2022-05-23 16:54:54 +0300

    lesson-6

--- lesson-6/src/index.js
+++ lesson-6/src/index.js
@@ -5,29 +5,10 @@ import App from "./App";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { createStore } from "redux";
+import { store } from "./redux/configStore";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-const initialState = {
-  count: 0,
-};
-const reducer = (state = initialState, action) => {
-  switch (action.type) {
-    case "add":
-      return {
-        ...state,
-        count: state.count + 1,
-      };
-    case "sub":
-      return {
-        ...state,
-        count: state.count - 1,
-      };
 
-    default:
-      return state;
-  }
-};
-const store = createStore(reducer);
 root.render(
   <BrowserRouter>
     <Provider store={store}>
